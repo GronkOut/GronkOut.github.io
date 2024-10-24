d87d4fc368d92495db151b2a2a1663a92cb96713
GronkOut <gronkout@ncsoft.com>
2024-10-25 07:34:17 +0900

    포트폴리오 정리

---
+++ scripts/index.js
@@ -0,0 +1,65 @@
+;(($) => {
+  const $fullpage = $('#fullpage');
+  let fp;
+
+  if ($fullpage.length) {
+    fp = new fullpage('#fullpage', {
+      licenseKey: '4A970FEE-847D4023-8572C79E-2838B2CF',
+      menu: '#menu',
+      navigation: true,
+      slidesNavigation: true,
+      loopHorizontal: false,
+      normalScrollElements: '.swiper, [section="option"]',
+      scrollOverflow: true,
+      scrollOverflowReset: true,
+      scrollOverflowResetKey: 'cGxheW5jLmNvbV9yMzVjMk55YjJ4c1QzWmxjbVpzYjNkU1pYTmxkQT09RTd4',
+      parallax: true,
+      parallaxKey: 'cGxheW5jLmNvbV85WTJjR0Z5WVd4c1lYZz05VW4=',
+      parallaxOptions: {
+        type: 'cover',
+        percentage: 50,
+        property: 'translate',
+      },
+    });
+  }
+
+  const $swiper = $('.swiper');
+
+  if ($swiper.length) {
+    $swiper.each((index, element) => {
+      const $element = $(element);
+      const $firstVideo = $element.find('.swiper-slide').eq(0).find('video');
+
+      new Swiper(element, {
+        slidesPerView: 'auto',
+        autoHeight: true,
+        pagination: {
+          el: $element.find('.swiper-pagination')[0],
+        },
+        on: {
+          init(swiper) {
+            if ($firstVideo.length) {
+              $firstVideo.on('loadedmetadata', () => {
+                swiper.update();
+              });
+            }
+          }
+        },
+      });
+    });
+  }
+
+  $('img').viewer({
+    title: false,
+    toolbar: false,
+    navbar: false,
+    viewed() {
+      fp.setAllowScrolling(false);
+      fp.setKeyboardScrolling(false);
+    },
+    hidden() {
+      fp.setAllowScrolling(true);
+      fp.setKeyboardScrolling(true);
+    }
+  });
+})(window.jQuery);
